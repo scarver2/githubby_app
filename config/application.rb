ef8e8acfc02d680c32f38c8c94b3ef65a7f7e4a5
Stan Carver II <stan@a1webconsulting.com>
2015-09-18 14:59:56 -0500

    DEV: changed app name

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Hubby
-  # Hubby Application
+module GitHubby
+  # GitHubby Application
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
