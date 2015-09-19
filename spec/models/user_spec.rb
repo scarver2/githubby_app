89d0f47e910b7caaa3245ff975579f8a78212f67
Stan Carver II <stan@a1webconsulting.com>
2015-09-19 15:46:22 -0500

    DEV: removed RSpec3 deprecations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    it { should respond_to :created_at }
+    it { should respond_to :name }
+    it { should respond_to :provider }
+    it { should respond_to :uid }
+    it { should respond_to :updated_at }
+  end
 end
