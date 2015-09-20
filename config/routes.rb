3e902418760eab2ebe74ed8630324f5af5075607
Stan Carver II <stan@a1webconsulting.com>
2015-09-20 08:52:33 -0500

    added in-app git log

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#failure'
-  get '/logout' => 'sessions#destroy', as: :logout
-  get '/search' => 'search#index', as: :search
+  get '/logout', to: 'sessions#destroy', as: :logout
+  get '/search', to: 'search#index', as: :search
+  get '/log', to: 'home#log', as: :log
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
