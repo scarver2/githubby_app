0726ef07b1b3bb8c4aaee582a26eb86ee3a39290
Stan Carver II <stan@a1webconsulting.com>
2015-09-20 00:33:10 -0500

    DEV: validate presence of OAuth info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 # User
 class User < ActiveRecord::Base
+  validates :provider, presence: true
+  validates :uid, presence: true, uniqueness: { scope: :provider }
 end
--- db/migrate/20150918233205_create_users.rb
+++ db/migrate/20150918233205_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :provider
-      t.string :uid
+      t.string :provider, null: false
+      t.string :uid, null: false
       t.string :name
-
       t.timestamps null: false
     end
+    add_index :users, [:provider, :uid], unique: true
   end
 end
