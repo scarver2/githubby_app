34b9d70fe3849b68f01c4dcf6db161e20ea4a976
Stan Carver II <stan@a1webconsulting.com>
2015-09-21 05:49:03 -0500

    fixed app version--heroku was returning nil

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,5 @@ module GitHubby
     config.active_record.raise_in_transactional_callbacks = true
   end
 
-  VERSION = `git describe --always` || `git tag` || ''
+  VERSION = [`git describe --always`, ENV['APPLICATION_VERSION']].delete_if(&:empty?).first
 end
