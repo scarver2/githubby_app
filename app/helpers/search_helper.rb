72df87ee133e35795343f09a6997e64086ad6da3
Stan Carver II <stan@a1webconsulting.com>
2015-09-21 01:51:45 -0500

    minor

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,7 +1,7 @@
 # SearchHelper
 module SearchHelper
   def search_path_for(search_form, attrs = {})
-    binding.pry
+    # binding.pry
     params = {
       page: attrs[:page] || search_form.page,
       q: attrs[:q] || search_form.q
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   APICache.store = APICache::DalliStore.new(Dalli::Client.new)
 end
